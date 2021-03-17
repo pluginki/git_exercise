155c1af64f822eeed8f5b6b99c6a5bf72d56a4db
kirill_merkulov <kirill_merkulov@epam.com>
2021-03-17 17:54:38 +0300

    Mir premium implemented

--- src/main/java/com/epam/git_practice/model/Cards.java
+++ src/main/java/com/epam/git_practice/model/Cards.java
@@ -4,12 +4,17 @@ public enum Cards {
 
     MIR {
         private static final long CLASSIC_BIN = 2200_0000_0000_000L;
+        private static final long PREMIUM_BIN = 2201_0000_0000_000L;
+
         @Override
         public long generateCardNumber(String cardType) {
             long cardNumber;
             switch (cardType.toLowerCase()) {
                 case "classic":
                     cardNumber = Cards.generatePartOfCardNumber(CLASSIC_BIN);
+                    break;
+                case "premium":
+                    cardNumber = Cards.generatePartOfCardNumber(PREMIUM_BIN);
                     break;
                 default:
                     throw new CardTypeWasNotFoundException();
