e68c6256d226b1eb61d2470885b681b4e88a548d
kirill_merkulov <kirill_merkulov@epam.com>
2021-03-17 16:42:09 +0300

    added card controller

---
+++ src/main/java/com/epam/git_practice/Application.java
@@ -0,0 +1,10 @@
+package com.epam.git_practice;
+
+import com.epam.git_practice.controller.Controller;
+
+public class Application {
+
+    public static void main(String[] args) {
+        new Controller().startApplication(args);
+    }
+}
--- src/main/java/com/epam/git_practice/view/View.java
+++ src/main/java/com/epam/git_practice/view/View.java
@@ -1,4 +1,11 @@
 package com.epam.git_practice.view;
 
 public class View {
+    private static final String OUTPUT_PATTERN = "%s %s %d%n";
+
+    public void showCardNumber(String paymentSystem, String cardType,
+                               long cardNumber) {
+        System.out.printf(OUTPUT_PATTERN, paymentSystem, cardType, cardNumber);
+    }
+
 }
